18803f80350eb2444b78601629320ea0f523c1bc
FranzGoAcquirely <franz@goacquirely.com>
2023-04-23 21:37:58 +0800

    Disabled debug 2

--- src/pages/api/account/settings/get.ts
+++ src/pages/api/account/settings/get.ts
@@ -12,45 +12,44 @@ export default async function handler(
     const session = await getServerSession(req,res,authentication) 
 
     if(req.method === "GET"){
-        const getSettings = await prisma.settings.findUnique({
-            where: {
-                user_settings_id: session?.user?.id
-            },
-            select: {
-                bg_video_mp4: true,
-                bg_video_webm: true
-            },
-        })  
-
-        const getArena = await prisma.arena.findFirst({
-            where: {
-                host_id: session?.user?.id
-            },
-            orderBy: [
-                {
-                    createdAt: 'desc'
-                }
-            ]
-        })  
-        res.status(200).json({ 
-            success: true,
-            settings: {
-                mp4: getSettings?.bg_video_mp4,
-                webm: getSettings?.bg_video_webm
-            },
-            user: {
-                id: session?.user?.id,
-                name: session?.user?.name,
-                role: session?.user?.role,
-                avatar: session?.user?.avatar
-            },
-            arena:{
-                id: getArena?.uid
-            }
-        })
         try{
             if(session){
-                
+                const getSettings = await prisma.settings.findUnique({
+                    where: {
+                        user_settings_id: session?.user?.id
+                    },
+                    select: {
+                        bg_video_mp4: true,
+                        bg_video_webm: true
+                    },
+                })  
+
+                const getArena = await prisma.arena.findFirst({
+                    where: {
+                        host_id: session?.user?.id
+                    },
+                    orderBy: [
+                        {
+                            createdAt: 'desc'
+                        }
+                    ]
+                })  
+                res.status(200).json({ 
+                    success: true,
+                    settings: {
+                        mp4: getSettings?.bg_video_mp4,
+                        webm: getSettings?.bg_video_webm
+                    },
+                    user: {
+                        id: session?.user?.id,
+                        name: session?.user?.name,
+                        role: session?.user?.role,
+                        avatar: session?.user?.avatar
+                    },
+                    arena:{
+                        id: getArena?.uid
+                    }
+                })
             }
         }
         catch(err) {
