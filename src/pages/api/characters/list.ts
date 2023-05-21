90648bffcd00a32f98365d936fe99dcf8827513f
FranzGoAcquirely <franz@goacquirely.com>
2023-05-22 00:00:55 +0800

    reverted back part 2

--- src/pages/api/characters/list.ts
+++ src/pages/api/characters/list.ts
@@ -1,55 +1,56 @@
-
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/prisma/client'
+import { createError } from 'micro';
+import { json, send } from 'micro';
 
 
+interface RequestBody {
+    page: string;
+}
+  
 export const config = {
     api: {
-      responseLimit: false,
+        bodyParser: false, 
     },
-}
-
+};
+  
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ) {
-
-    if(req.method === "POST"){
-        try{
-            let charactersQuery = null;
-            if(req.body.page === "Settings"){
-                charactersQuery = await prisma.characters.findMany({
-                    orderBy: [
-                        {
-                          name: 'asc',
-                        }
-                    ]
-                })
-            }
-            else{
-                charactersQuery = await prisma.characters.findMany({
-                    where:{
-                        is_visible: true
-                    },
-                    orderBy: [
-                        {
-                          name: 'asc',
-                        }
-                    ]
-                })
-            }
-            
-            res.status(200).json({ 
-                success: true,
-                list: charactersQuery
-            })
-        }
-        catch(error){
-            res.status(403).json({ 
-                success: false,
-                error: "Error on fetch characters"
-            })
-        }
-    }   
-}
+    if (req.method === 'POST') {
+      try {
+        
+        const body = (await json(req)) as RequestBody;
   
+        let charactersQuery = null;
+        if (body.page === 'Settings') {
+          charactersQuery = await prisma.characters.findMany({
+            orderBy: [
+              {
+                name: 'asc',
+              },
+            ],
+          });
+        } else {
+          charactersQuery = await prisma.characters.findMany({
+            where: {
+              is_visible: true,
+            },
+            orderBy: [
+              {
+                name: 'asc',
+              },
+            ],
+          });
+        }
+  
+        send(res, 200, {
+          success: true,
+          list: charactersQuery,
+        });
+      } catch (error: unknown) {
+        throw createError(403, 'Error on fetching characters',  error as Error);
+      }
+    }
+  }
