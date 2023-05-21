2789ce305a5abbcf007a29a1929c1bb68043b78d
FranzGoAcquirely <franz@goacquirely.com>
2023-05-21 23:33:36 +0800

    Fix huge data response character list 3

--- src/pages/arena/[arenaID]/draft/[draftID].tsx
+++ src/pages/arena/[arenaID]/draft/[draftID].tsx
@@ -1181,6 +1181,9 @@ export const getStaticProps: GetStaticProps<
 > = async () => {
   const prisma = new PrismaClient();
   const character_list = await prisma.characters.findMany({
+    where: {
+      is_visible: true,
+    },
     orderBy: [
       {
         name: "asc",
