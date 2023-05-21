4a7358151938a614824976dc76632cf554f0c095
FranzGoAcquirely <franz@goacquirely.com>
2023-05-21 23:47:32 +0800

    reverted back

--- src/pages/arena/[arenaID]/draft/[draftID].tsx
+++ src/pages/arena/[arenaID]/draft/[draftID].tsx
@@ -7,7 +7,7 @@ import ModalBoss from "@/components/ModalBoss";
 import WinnerModal from "@/components/WinnerModal";
 import Head from "next/head";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { motion } from "framer-motion";
@@ -20,9 +20,6 @@ import { useDraftStore } from "@/libs/store/draft";
 import { ModalCharacterPickBlur } from "@/src/styles/CharacterPick";
 import {
   CharacterDraftPayloadProps,
-  CharacterInfoProps,
-  CharacterListProps,
-  CharacterListPropsDraft,
   DraftInfoProps,
   TimerUpdateProps,
 } from "@/libs/helpers/types";
@@ -62,9 +59,8 @@ import {
 } from "@/libs/providers/draft";
 import { timerStore } from "@/libs/store/timer";
 import { socket } from "@/libs/providers/socket";
-import { PrismaClient } from "@prisma/client";
 
-const Drafting: NextPage = ({ character_list }: any) => {
+const Drafting: NextPage = () => {
   const { state, setBackgroundVid } = useUserData();
   const router = useRouter();
 
@@ -280,6 +276,23 @@ const Drafting: NextPage = ({ character_list }: any) => {
     },
   });
 
+  const characterListQuery = useQuery({
+    queryFn: async () => {
+      const listResponse = await api.post("/characters/list", {
+        page: "Character List",
+      });
+      return listResponse.data.list;
+    },
+    queryKey: ["characterDraftList", router.query.draftID],
+    refetchOnWindowFocus: false,
+    onSuccess: (data) => {
+      for (const character of data) {
+        character.isPicked = false;
+      }
+      setCharacterDraftListUpdate(characterDraft, data);
+    },
+  });
+
   useQuery({
     queryFn: async () => {
       const listResponse = await api.post("/arena/draft/update", {
@@ -346,15 +359,6 @@ const Drafting: NextPage = ({ character_list }: any) => {
       socket.emit("redraft", data.socket);
     },
   });
-
-  useEffect(() => {
-    if (character_list) {
-      for (const character of character_list) {
-        character.isPicked = false;
-      }
-      setCharacterDraftListUpdate(characterDraft, character_list);
-    }
-  }, []);
 
   useEffect(() => {
     const timerFeat = (data: any) => {
@@ -709,7 +713,7 @@ const Drafting: NextPage = ({ character_list }: any) => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Escape") {
         onToggleCharacterPickModal();
-        setCharactersList(character_list);
+        setCharactersList(characterListQuery.data);
       }
     };
 
@@ -845,7 +849,7 @@ const Drafting: NextPage = ({ character_list }: any) => {
         <CharacterDraft
           statusCharacterModal={applyCharacterModal}
           onCloseCharacterModal={onToggleCharacterPickModal}
-          characterListQuery={character_list}
+          characterListQuery={characterListQuery}
           timer={timer}
           onCharacterPick={onCharacterDraftChoose}
           state={state}
@@ -1160,41 +1164,4 @@ const Drafting: NextPage = ({ character_list }: any) => {
     </>
   );
 };
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const arenaIDs = ["another-arena-id", "..."];
-  const draftIDs = ["another-draft-id", "..."];
-
-  // Generate the dynamic paths using the valid arenaIDs
-  const paths = arenaIDs.flatMap((arenaID) =>
-    draftIDs.map((draftID) => ({ params: { arenaID, draftID } }))
-  );
-
-  return {
-    paths,
-    fallback: "blocking",
-  };
-};
-
-export const getStaticProps: GetStaticProps<
-  CharacterListPropsDraft
-> = async () => {
-  const prisma = new PrismaClient();
-  const character_list = await prisma.characters.findMany({
-    where: {
-      is_visible: true,
-    },
-    orderBy: [
-      {
-        name: "asc",
-      },
-    ],
-  });
-
-  return {
-    props: { character_list },
-    revalidate: 10,
-  };
-};
-
 export default Drafting;
