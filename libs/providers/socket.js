0e10f1bf0c8185c5ca3e0e117ca2e8d7102570df
Marlito Dungog <marlitobdungod@gmail.com>
2023-10-23 03:10:23 +0800

    Socket changes revert

--- libs/providers/socket.js
+++ libs/providers/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 let BASE_URL = "";
 if (process.env.PRODUCTION_TYPE == "staging") {
-  BASE_URL = "https://endgame-socket.onrender.com/";
+  BASE_URL = "http://localhost:5000/";
 } else {
   BASE_URL = "https://endgame-socket.onrender.com/";
 }
