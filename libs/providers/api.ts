76d8c332ffd24b219d291e34d0279c0bdec8f276
Marlito Dungog <marlitobdungod@gmail.com>
2023-11-02 05:22:12 +0800

    Axios Test

--- libs/providers/api.ts
+++ libs/providers/api.ts
@@ -5,7 +5,8 @@ if(process.env.PRODUCTION_TYPE == 'staging'){
   BASE_URL = "http://localhost:3000/api";
 }
 else{
-  BASE_URL = "https://endgame.paimonsandwich.gg/api";
+  // BASE_URL = "https://endgame.paimonsandwich.gg/api";
+  BASE_URL = "http://localhost:3000/api";
 }
 
 
